72ee5b07a9e9091a2ed5bf24009c5138790cce37
Emanuele Nencioni <emanuele.nencioni@stud.unifi.it>
2022-08-12 14:33:34 +0200

    implemented class DomainModel

--- applicationModel/DomainModel.java
+++ applicationModel/DomainModel.java
@@ -8,10 +8,57 @@ import java.util.*;
 public class DomainModel {
 
     /**
+     * @param es Array of entity
+     */
+    public DomainModel(Entity[] es) {
+        entities = new ArrayList<>();
+        for(int i=0;i<es.length;i++){
+            entities.add(es[i]);
+        }
+    }
+    /**
+     * Copy constructor
+     * @param es array list of entity
+     */
+    public DomainModel(ArrayList<Entity> es) {
+        entities = new ArrayList<>();
+        for(int i=0;i<es.size();i++){
+            entities.add(es.get(i));
+        }
+    }
+    /**
      * Default constructor
      */
-    public DomainModel() {
+    public DomainModel(){
+        entities = new ArrayList<Entity>();
     }
 
+    public void addEntity(Entity e){
+        entities.add(e);
+    }
+    /**
+     * 
+     * @param idx the index of the entity in the ArrayList, 
+     * @return the Entity or null if the idx is out the range the ArrayList's size
+     */
+    public Entity getEntity(int idx){
+        if (idx < entities.size() && idx >= 0){
+            return entities.get(idx);
+        }
+        return  null;
+    }
+    /**
+     * delete the entity given the idx
+     * @param idx the index of the entity in the ArrayList
+     * @return the entity that has been deleted
+     */
+    public Entity deleteEntity(int idx){
+        if (idx < entities.size() && idx <= 0){
+            return  entities.remove(idx);
+        }
+        return null;
+    }
+
+    private ArrayList<Entity> entities;
 
 }
