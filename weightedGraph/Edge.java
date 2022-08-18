9b56361280b112b8fdce81960de07a332de810d6
Sode99 <giacomo.sodero@gmail.com>
2022-08-18 22:29:05 +0200

    implemented classess vertex, edge, graph

--- weightedGraph/Edge.java
+++ weightedGraph/Edge.java
@@ -1,43 +1,27 @@
 package weightedGraph;
 
-import java.util.*;
-
 /**
  * 
  */
 public class Edge {
 
-    /**
-     * Default constructor
-     */
-    public Edge() {
+    
+    public Edge(float weight, Vertex v1, Vertex v2) {
+        this.weight = weight;
+        this.vertex1 = v1;
+        this.vertex2 = v2;
     }
-
-    /**
-     * 
-     */
-    private float weight;
-
-    /**
-     * 
-     */
-    private Vertex vertex1;
-
-    /**
-     * 
-     */
-    private Vertex vertex2;
-
-
-
 
 
     /**
      * @param Vertex v 
      * @return
      */
-    public Vertex getConnVertex(void Vertex v) {
-        // TODO implement here
+    public Vertex getConnVertex(Vertex v) {
+        if (v.equals(vertex1))
+            return vertex2;
+        if (v.equals(vertex2))
+            return vertex1;
         return null;
     }
 
@@ -45,8 +29,23 @@ public class Edge {
      * @return
      */
     public float getWeight() {
-        // TODO implement here
-        return 0.0f;
+        return weight;
     }
+
+
+    public boolean equals(Object obj) {
+        if (obj instanceof Edge) {
+            Edge e = (Edge) obj;
+            return e.weight == this.weight
+                && e.vertex1.equals(this.vertex1)
+                && e.vertex2.equals(this.vertex2);
+        }
+        return false;
+    }
+    
+    private float weight;
+    private Vertex vertex1;
+    private Vertex vertex2;
+
 
 }
