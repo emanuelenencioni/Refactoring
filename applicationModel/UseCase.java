1ff15572699d7548551dbec9d36533f7950aa794
Emanuele Nencioni <emanuele.nencioni@stud.unifi.it>
2022-08-27 16:54:34 +0200

    implemented use case

--- applicationModel/UseCase.java
+++ applicationModel/UseCase.java
@@ -8,29 +8,63 @@ import java.util.*;
 public class UseCase extends ApplicationAbstraction {
 
     /**
-     * Default constructor
+     * 
+     * @param id the id of the use case
      */
-    public UseCase() {
+    public UseCase(String id){
+        super(id, 1, null);
+    }
+    /**
+     * 
+     * @param id the id of the use case
+     * @param f the frequency of the use case
+     */
+    public UseCase(String id, float f) {
+        super(id, f, null);
+    }
+    /**
+     * 
+     * @param id the id of the use case
+     * @param f the frequency of the use case
+     * @param aa list of  End Points
+     */
+    public UseCase(String id, float f, ArrayList<EndPoint> epl){
+        this(id,f);
+        this.endPointList = new ArrayList<EndPoint>();
+        for(int i = 0; i< epl.size(); i++)
+            this.endPointList.add(epl.get(i));
     }
 
+    /**
+     * 
+     * @param id the id of the use case
+     * @param f the frequency of the use case
+     * @param aa list of  End Points
+     * @param strat the strategy to build the matrices
+     */
+    public UseCase(String id, float f,ArrayList<EndPoint> epl, BuildCoMatStrategy strat){
+        this(id,f,epl);
+        this.strategy = strat;
+    }
 
-
-
+    @Override
+    public void buildMatrices() {
+        ArrayList<ApplicationAbstraction> aa = new ArrayList<>();
+        for (EndPoint ep : endPointList) {
+            aa.add(ep);
+        }
+        ArrayList<CoOccurrenceMatrix>  list = buildCoMat(aa);
+        for(CoOccurrenceMatrix coc : list)
+            this.mapper.put(coc.getType(), coc);
+    }
 
     /**
      * 
      */
-    public void buildCoMat(ArrayList<EndPoint> e): ArrayList<CoOccurrenceMatrix>() {
-        // TODO implement here
-    }
+   @Override
+   protected ArrayList<CoOccurrenceMatrix> buildCoMat(ArrayList<ApplicationAbstraction> aa) {//TODO serve davvero?
+       return strategy.buildCoMat(aa);
+   }
 
-    /**
-     * @param BuildCoMatStrategyUC strategyId 
-     * @return
-     */
-    public void setStrategy(void BuildCoMatStrategyUC strategyId) {
-        // TODO implement here
-        return null;
-    }
-
+   private ArrayList<EndPoint> endPointList;
 }
