acb066df3064cf8b2df67091e88ae1ccb0a844a4
Ric8 <fedemagio8@gmail.com>
2023-01-21 17:26:19 +0100

    Implemented InputSGF, 1 fix to do

--- src/manager/InputSGF.java
+++ src/manager/InputSGF.java
@@ -1,5 +1,7 @@
 package src.manager;
 
+import java.util.Scanner;
+
 public class InputSGF implements SimplifyGraphFactory{
 
 
@@ -16,12 +18,68 @@ public class InputSGF implements SimplifyGraphFactory{
      */
     public SimplifyGraphStrategy createSimplifyGraphStrategy(SimplifyGraphType t) {
 
-        switch (t) {
+        switch (t) 
+        {
             case MSTClustering:
+                // System.out.println("You chose MSTClustering to Simplify Graph, insert values for n and s greater than 0.");
+
+                // //INSERT N
+                // Scanner scan1 = new Scanner(System.in);
+                // boolean inputOk = false;
+            
+                // int n = 0;
+                // while (!inputOk) {
+                //     System.out.println("Insert n");
+                //     if (!scan1.hasNextInt()) {
+                //         System.out.println("Input is not valid: not a number");
+                //         scan1.next();
+                //     }
+                //     else {
+                //         n = scan1.nextInt();
+                //         if (n <= 0){
+                //             System.out.println("Input is not valid: number must be greater than 0");
+                //         }
+                //         else {
+                //             inputOk = true;
+                //         }
+                //     }
+                // }
+                // scan1.close();
+
+
+                // // INSERT S
+                // System.out.println("Insert s: ");
+                // Scanner scan2 = new Scanner(System.in);
+
+                
+                // inputOk = false;
+            
+                // int s = 0;
+                // while (!inputOk) {
+                //     System.out.println("Enter a number greater than 0");
+                //     if (!scan2.hasNextInt()) {
+                //         System.out.println("Input is not valid: not a number");
+                //         scan2.next();
+                //     }
+                //     else {
+                //         s = scan2.nextInt();
+                //         if (s <= 0){
+                //             System.out.println("Input is not valid: number must be greater than 0");
+                //         }
+                //         else{
+                //             inputOk = true;
+                //         }
+                //     }
+                // }
+                // scan2.close();
+                // FIXME non funziona
+
+                // return new MSTClustering(n, s);
                 return new MSTClustering();
 
             default:
-                return null;}
+                return null;
+        }
 
     }
 
