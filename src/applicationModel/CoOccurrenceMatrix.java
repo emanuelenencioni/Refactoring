9fec5cbed0bbd6e6811b995ac857d063a1d7c811
Emanuele Nencioni <emanuele.nencioni@stud.unifi.it>
2023-01-23 11:05:25 +0100

    now getValue return null

--- src/applicationModel/CoOccurrenceMatrix.java
+++ src/applicationModel/CoOccurrenceMatrix.java
@@ -89,7 +89,7 @@ public class CoOccurrenceMatrix {
         Integer x = mapper.get(source);
         Integer y = mapper.get(dest);
         if(x == null || y == null)
-            return 0.f;
+            return null;
         else
             return cOMatrix[x][y];
     }
